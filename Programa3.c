7c62d40a0c109775a2d79ec7dbf9dc5ce8f252f8
han-k31 <hannasgiron@gmail.com>
2025-10-09 14:03:59 -0600

    Create Programa3.c

---
+++ Programa3.c
@@ -0,0 +1,19 @@
+/*Girón Knepp Hanna Sofía
+Programa 3*/
+
+#include <stdio.h>
+int main()
+{
+  int num;
+  printf("Ingrese un número:\n");
+  scanf("%d",&num);
+  if ( num%2 == 0 )
+  {
+    printf("El número %d es par.\n",num);
+  }
+  else
+  {
+    printf("El número %d es impar.\n",num);
+  }
+  return 0;
+}
