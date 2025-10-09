5ecc5b8cb0fba2ee0dd087c607de7f90111fa0da
han-k31 <hannasgiron@gmail.com>
2025-10-09 14:26:26 -0600

    Update Programa4.c

--- Programa4.c
+++ Programa4.c
@@ -16,32 +16,32 @@ int main()
     else
     {
       if (uno > tres)
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", uno, tres, dos);
+      }
+      else
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
+      }
+    }
+  }
+  else
+  {
+    if (dos > tres)
     {
-      printf("%d es mayor a %d que es mayor a %d\n", uno, tres, dos);
+      if (tres > uno)
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", dos, tres, uno);
+      }
+      else
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
+      }
     }
     else
     {
-      printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
+       printf("%d es mayor a %d que es mayor a %d\n", tres, dos, uno);
     }
-    }
-  }
-  else
-  {
-  if (dos > tres)
-  {
-  if (tres > uno)
-  {
-      printf("%d es mayor a %d que es mayor a %d\n", dos, tres, uno);
-  }
-  else
-  {
-    printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
-  }
-  }
-  else
-  {
-    printf("%d es mayor a %d que es mayor a %d\n", tres, dos, uno);
-  }
   }
 return 0;
 }
